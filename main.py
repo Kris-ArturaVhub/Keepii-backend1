d3d53242ef836ccd588238a0fb88fab8df19c52c
Kris-ArturaVhub <lckhanh2010@gmail.com>
2026-07-03 17:56:48 +0700

    Update database user password
    Changed the password for database user to a new value.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 
 # 🌐 Khai báo mật khẩu riêng ra để mã hóa ký tự đặc biệt (@)
 username = "lekhanh230893_db_user"
-password = "1JWuq0REXGoWLbJy" 
+password = "1234567890qwertyuiop" 
 
 # Sử dụng quote_plus để biến dấu @ thành định dạng mã hóa an toàn (%40)
 escaped_password = urllib.parse.quote_plus(password)
